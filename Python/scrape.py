5d657b1c3eeb7b4c9622049003ab8e21628eb5cd
SpiDher <penivera655@gmail.com>
2024-09-11 17:02:21 +0100

    updated scrape and practice files

--- Python/scrape.py
+++ Python/scrape.py
@@ -2,7 +2,7 @@ import requests
 import json
 import sys
 from practice import tracks
-tracks()
+
 def search(t):
 	if len(t) < 6:
 		sys.exit()
@@ -10,6 +10,7 @@ def search(t):
 		response = requests.get('https://itunes.apple.com/search?entity=song&limit=10&term=' + t)
 		# print(json.dumps(response.json(), indent=2))
 		data = response.json()
+		print(data)
 		hold =[]
 		for info in data['results']:
 			hold.append(info['trackName'])
@@ -20,5 +21,6 @@ def ok():
 def main():
 	search(sys.argv[1])
 	ok()
+
 if __name__ == "__main__":
 	main()
