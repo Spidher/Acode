c9eed55b2ddca3fae4b795e96d2bf3cb506aa303
SpiDher <penivera655@gmail.com>
2024-09-16 21:12:08 +0100

    Update to output

--- Python/xrequest.py
+++ Python/xrequest.py
@@ -1,44 +1,8 @@
 import sys
 import requests
-<<<<<<< HEAD
 import csv
 import json
-def get_key():
-	with open('X_keys.csv', 'r') as file:
-		reader = csv.DictReader(file)
-		for row in reader:
-			if row['name'] =='bearer_token' :
-				return row['token']
- 			
-Header = {'Authorization': f'Bearer {get_key()}'}
-search_query, max_results = sys.argv[1], int(sys.argv[2])
-url = 'https://api.twitter.com/2/tweets/search/recent'
-param ={
-	'query': search_query,
- 	'max_results': max_results,
- 	'expansion' : 'reply_to_user_id',
- 	'tweet.fields' : 'attachments',
- 	'user.fields' : 'username',
- 	'tweet.fields' : 'created_at,text'
-}
-def resp():
-	response = requests.get(url,headers = Header,params = param)
-	if response.status_code == 200:
-		return f'Succesful {json.dump(response.json, indent =2)}'
-	else:
-		return 'Error sending request'
-
-def main():
-  print(resp())
-
-
-
-if __name__ == '__main__':
-  main()
-=======
 from requests_oauthlib import OAuth1
-import csv
-import json
 
 
 post_url = 'https://api.twitter.com/2/tweets'
@@ -94,7 +58,7 @@ def fselector():
       else:
         post_tweet(sys.argv[2])
     elif sys.argv[1] == 'd':
-      print(post_delete(sys.argv[2]))
+      post_delete(sys.argv[2])
     else:
       print(''' Enter *ul* to perform a user lookup
       
@@ -204,21 +168,24 @@ def post_outsave(response):
   else:
     print(response.text)
 
+def post_delete(tweet_id):
+  # Construct the correct URL with the tweet ID
+  d_url = f'https://api.twitter.com/2/tweets/{int(tweet_id)}'
+  
+  # Make the delete request with OAuth authentication
+  response = requests.delete(d_url, auth=authenticate())
 
-def post_delete(id):
-  d_url = f'https://api.twitter.com/2/tweets/{id}'
-  response = requests.delete(d_url, auth = authenticate())
-  if response.status_code == 201 or response.status_code == 200:
+  # Check for a successful response (status code 200)
+  if response.status_code == 200:
+    print("Tweet deleted successfully!")
+    response = response.json()
     if 'data' in response:
-      for key,value in response['data']:
-        result = f'Post deleted, server response\n{key,value}'
-        print(result)
-      return result
+      for key,value in response['data'].items():
+        print(f'{key.title()}:{value}')
   else:
     print(f'Failed to delete tweet: {response.status_code} - {response.text}')
-    return f'Failed to delete tweet: {response.status_code} - {response.text}'
-    
-  
+    return None
+
   
 def main():
   fetch_auth_param()
@@ -227,4 +194,4 @@ def main():
   
 if __name__ == '__main__':
   main()
->>>>>>> xrequest
+
